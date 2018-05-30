437d7a0abc6292e1df468f601772988be37d2502
donohue.d.f@gmail.com <donohue.d.f@gmail.com>
2018-05-29 22:45:38 -0600

    Feat: Initial commit of blog
    Commit includes django proejct and blog app with minimal functionality

---
+++ exhibit/exhibit/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+from blog.models import Post
+
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')[:3]
+        return context
